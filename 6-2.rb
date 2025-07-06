1d0fff09ba31402f3c5b9e5b2bcfb920add5e381
byeongcheollim <lim6112j@gmail.com>
2025-07-06 23:04:07 +0900

    feat: add first letter hash and print text

--- 6-2.rb
+++ 6-2.rb
@@ -6,3 +6,9 @@ text = 'The rain in Spain falls mainly in the plain.'
 word_count_hash = Hash.new 0
 text.split(/\W+/).each { |word| word_count_hash[word.downcase] += 1 }
 pp word_count_hash
+pp text
+first_letter_hash = Hash.new []
+text.split(/\W+/).each do |word|
+  first_letter_hash[word[0, 1].downcase] << word
+end
+pp first_letter_hash
