20c991fbeb006aa5a6cea122a61a5ef032c49fb9
byeongcheollim <lim6112j@gmail.com>
2025-07-06 20:04:49 +0900

    feat: add histogram and graph drawing functionality

--- 6-12.rb
+++ 6-12.rb
@@ -9,5 +9,15 @@ module Enumerable
     end
   end
 end
+survey_results = { 'Alice' => :red, 'Bob' => :green, 'Carol' => :green,
+                   'Mallory' => :blue }
 
 puts [1, 2, 2, 2, 5, 5, 7, 8, 8, 8].to_histogram
+def draw_graph(histogram, char = '*')
+  pairs = histogram.keys.collect { |x| [x.to_s, histogram[x]] }.sort
+  largest_key_size = pairs.max { |x, y| x[0].size <=> y[0].size }[0].size
+  pairs.inject(0) do |s, kv|
+    s << "#{kv[0].ljust(largest_key_size)} |#{char * kv[1]}\n"
+  end
+end
+puts draw_graph(survey_results.values.to_histogram)
