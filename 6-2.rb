39380a17b645bdf1b3eb2d6997357e3903df107e
byeongcheollim <lim6112j@gmail.com>
2025-07-06 23:15:33 +0900

    chore: update frozen_string_literal and add first letter hash output

--- 6-2.rb
+++ 6-2.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 require 'pp'
 text = 'The rain in Spain falls mainly in the plain.'
@@ -7,8 +7,9 @@ word_count_hash = Hash.new 0
 text.split(/\W+/).each { |word| word_count_hash[word.downcase] += 1 }
 pp word_count_hash
 pp text
-first_letter_hash = Hash.new { |h, k| h[k] = [] }
+first_letter_hash = Hash.new []
 text.split(/\W+/).each do |word|
   first_letter_hash[word[0, 1].downcase] << word
 end
+pp first_letter_hash['a']
 pp first_letter_hash
